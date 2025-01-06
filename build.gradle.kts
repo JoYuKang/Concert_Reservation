9453f8438ae0b3286645ff50eac196aca676d980
YU KANG JO <46198324+JoYuKang@users.noreply.github.com>
2025-01-06 21:35:22 +0900

    환경 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.projectlombok:lombok")
+	implementation("org.springdoc:springdoc-openapi-ui:1.6.11")
 
     // DB
 	runtimeOnly("com.mysql:mysql-connector-j")
