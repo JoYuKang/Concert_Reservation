0002c3c47f29f34dceb4f50d6cc12fde5c78c5a1
YU KANG JO <46198324+JoYuKang@users.noreply.github.com>
2025-02-01 20:25:27 +0900

    refactor : 콘서트 Paging 기능 추가

--- src/main/java/kr/hhplus/be/server/concert/infrastructure/ConcertJpaRepository.java
+++ src/main/java/kr/hhplus/be/server/concert/infrastructure/ConcertJpaRepository.java
@@ -1,17 +1,16 @@
 package kr.hhplus.be.server.concert.infrastructure;
 
 import kr.hhplus.be.server.concert.domain.Concert;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.domain.Pageable;
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 public interface ConcertJpaRepository extends JpaRepository<Concert, Long> {
 
-    List<Concert> findByTitleContaining(String title);
+    Page<Concert> findByTitleContaining(String title, Pageable pageable);
 
-    List<Concert> findByConcertDate(LocalDate date);
+    Page<Concert> findByConcertDate(LocalDate date, Pageable pageable);
 
 }
