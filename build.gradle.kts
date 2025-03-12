a1428d3e0b2081e9f5a18a2bf2b37e4cc4a2d2ca
YU KANG JO <46198324+JoYuKang@users.noreply.github.com>
2025-03-12 16:12:10 +0900

    build : redisson config 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 	implementation("org.springframework.boot:spring-boot-starter-cache")
 	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.2")
+	implementation("org.redisson:redisson-spring-boot-starter:3.16.1")
     // DB
 	runtimeOnly("com.mysql:mysql-connector-j")
 
